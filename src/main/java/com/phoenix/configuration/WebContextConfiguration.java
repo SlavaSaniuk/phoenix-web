a025e8ce0264a215293ebab815ac55c2e9f15c32
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-06-13 14:44:43 +0300

    Add static resources paths

--- src/main/java/com/phoenix/configuration/WebContextConfiguration.java
+++ src/main/java/com/phoenix/configuration/WebContextConfiguration.java
@@ -3,9 +3,7 @@ package com.phoenix.configuration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
@@ -22,4 +20,32 @@ public class WebContextConfiguration implements WebMvcConfigurer {
         configurer.enable();
     }
 
+    /**
+     * Add handlers to serve static resources such as images, js, and, css
+     * files from specific locations under web application root, the classpath,
+     * and others.
+     */
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //Add styles to views as static resource
+        registry.addResourceHandler("/styles/**").addResourceLocations("classpath:static/style/");
+        //Add javascript libraries to view
+        registry.addResourceHandler("/libs/**").addResourceLocations("classpath:static/lib/");
+        //Add images to view as static resources
+        registry.addResourceHandler("/img/**").addResourceLocations("classpath:static/img/");
+        //Add localization messages files to thymeleaf message source
+        registry.addResourceHandler("/lang/**").addResourceLocations("classpath:static/lang/");
+    }
+
+    /**
+     * Configure simple automated controllers pre-configured with the response
+     * status code and/or a view to render the response body. This is useful in
+     * cases where there is no need for custom controller logic -- e.g. render a
+     * home page, perform simple site URL redirects, return a 404 status with
+     * HTML content, a 204 with no content, and more.
+     */
+    public void addViewControllers(ViewControllerRegistry registry) {
+        //Redirect from root to view page
+       registry.addRedirectViewController("/", "/login");
+       registry.addRedirectViewController("/index", "/login");
+    }
 }
