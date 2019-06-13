fb5bb05e2d810518f53287b75da1c88c95ec427e
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-06-14 02:34:54 +0300

    Main root context configuration class.

--- src/main/java/com/phoenix/configuration/RootContextConfiguration.java
+++ src/main/java/com/phoenix/configuration/RootContextConfiguration.java
@@ -2,6 +2,12 @@ package com.phoenix.configuration;
 
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Main Spring web application root context configuration class.
+ * Class has all non web beans definition and import all configuration classes distributed by modules(packages).
+ * Root context has all application business logic, persistence configuration with beans definition,
+ * all services and its configurations.
+ */
 @Configuration
 public class RootContextConfiguration {
 
