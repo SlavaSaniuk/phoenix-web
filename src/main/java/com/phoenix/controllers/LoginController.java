35ec16346fe24e261083f83dca876abd76965157
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-06-14 02:39:45 +0300

    Login page controller

--- src/main/java/com/phoenix/controllers/LoginController.java
+++ src/main/java/com/phoenix/controllers/LoginController.java
@@ -5,10 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * {@link LoginController} class is a web controller class (annotated with {@link Controller} annotation)
+ * which return view name of login page (login.html) and handing login request (POST method).
+ */
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 
+    /**
+     * Method handing "GET" request to '/login' URL and return
+     * login page view name.
+     * @return - Login page view name ("login").
+     */
     @GetMapping
     public ModelAndView getLoginPage() {
         ModelAndView mav = new ModelAndView();
