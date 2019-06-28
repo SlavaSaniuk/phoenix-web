a728a35543ce506ab5e2b3c0d75b55de1100365d
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-06-28 10:20:14 +0300

    added constraints on fields and setters.

--- src/main/java/com/phoenix/models/Account.java
+++ src/main/java/com/phoenix/models/Account.java
@@ -4,6 +4,9 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "account", schema="phoenix")
@@ -11,11 +14,28 @@ public class Account {
 
     @Id
     @Column(name = "account_id", nullable = false)
+    @NotNull
     private int account_id;
 
     @Column(name = "account_email", nullable = false, unique = true)
+    @NotNull(message = "Account email field must not be null.")
+    @NotEmpty(message = "Account email field must not be empty")
     private String account_email;
 
     @Column(name = "account_password", nullable = false)
+    @NotNull
+    @NotEmpty
     private String account_password;
+
+
+    //Getters and setters
+    public int getAccountId() {        return account_id;    }
+
+    public String getAccountEmail() {        return account_email;    }
+    public void setAccountEmail(@Valid String account_email)  {
+        this.account_email = account_email;
+    }
+
+    public String getAccountPassword() {        return account_password;    }
+    public void setAccountPassword(@Valid String account_password) {        this.account_password = account_password;    }
 }
