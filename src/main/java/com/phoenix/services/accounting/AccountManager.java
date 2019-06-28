57625ab7cef832ec78bf29f018537d094aff7c3a
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-06-28 14:59:35 +0300

    Worked account management service

--- src/main/java/com/phoenix/services/accounting/AccountManager.java
+++ src/main/java/com/phoenix/services/accounting/AccountManager.java
@@ -6,6 +6,8 @@ import com.phoenix.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityExistsException;
+
 @Service
 public class AccountManager implements AccountManagementService {
 
@@ -18,7 +20,13 @@ public class AccountManager implements AccountManagementService {
     }
 
     @Override
-    public int registerAccount(Account a_account, User a_user) {
-        return 0;
+    public int registerAccount(Account a_account, User a_user) throws EntityExistsException{
+
+        //Found account with same name
+        Account founded = this.repository.findAccountByAccountEmail(a_account.getAccountEmail());
+        if (founded != null) throw new EntityExistsException("Account already registered.");
+
+        //Persist account
+        return this.repository.save(a_account).getAccountId();
     }
 }
