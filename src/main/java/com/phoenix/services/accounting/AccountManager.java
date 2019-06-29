cb3a0cda4fccd7f17ceaa035b21010e95e389858
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-06-29 17:02:34 +0300

    Worked AccountManagent service

--- src/main/java/com/phoenix/services/accounting/AccountManager.java
+++ src/main/java/com/phoenix/services/accounting/AccountManager.java
@@ -3,6 +3,7 @@ package com.phoenix.services.accounting;
 import com.phoenix.models.Account;
 import com.phoenix.models.User;
 import com.phoenix.repositories.AccountRepository;
+import com.phoenix.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,9 @@ public class AccountManager implements AccountManagementService {
     private AccountRepository repository;
 
     @Autowired
+    private UserRepository u_repo;
+
+    @Autowired
     public AccountManager(AccountRepository a_repository) {
             this.repository = a_repository;
     }
@@ -25,6 +29,11 @@ public class AccountManager implements AccountManagementService {
         //Found account with same name
         Account founded = this.repository.findAccountByAccountEmail(a_account.getAccountEmail());
         if (founded != null) throw new EntityExistsException("Account already registered.");
+
+        a_user = this.u_repo.save(a_user);
+
+        a_account.setAccountOwner(a_user);
+        a_user.setUserAccount(a_account);
 
         //Persist account
         return this.repository.save(a_account).getAccountId();
