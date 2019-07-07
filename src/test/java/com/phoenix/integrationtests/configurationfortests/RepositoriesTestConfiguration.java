f7c69db5c45d4a9c1a9c347e1a71986a2462a63b
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-07-07 13:52:01 +0300

    Added configuration files for test

---
+++ src/test/java/com/phoenix/integrationtests/configurationfortests/RepositoriesTestConfiguration.java
@@ -0,0 +1,9 @@
+package com.phoenix.integrationtests.configurationfortests;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories("com.phoenix.repositories")
+public class RepositoriesTestConfiguration {
+
+
+}
