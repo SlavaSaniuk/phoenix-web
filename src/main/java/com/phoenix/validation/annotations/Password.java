a9c75ee581ae6c65f756fc20286f3405a074f0b1
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-07-13 08:08:48 +0300

    Password annotation

--- src/main/java/com/phoenix/validation/annotations/Password.java
+++ src/main/java/com/phoenix/validation/annotations/Password.java
@@ -9,19 +9,53 @@ import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.constraints.NotEmpty;
 
+/**
+ * {@link Password} annotation used to mark password fields for validation. Password constraint may be defined
+ * in "security.properties" file with "com.phoenix.security.password.*" keys or with this annotations methods.
+ * {@link PasswordConstraintValidator} class holds validation logic for password fields.
+ * Note: Properties defined in "security.properties" prefer than annotations properties.
+ */
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = PasswordConstraintValidator.class)
 @NotEmpty(message = "{notempty.Account.password}")
 public @interface Password {
 
+    /**
+     * Default validation message if password field is not valid.
+     * @return {@link String} password error message.
+     */
     String message() default "Password field is invalid";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 
+    /**
+     * Minimum password length in characters. Default is 8 characters.
+     * @return - {@link Integer} minimum password length.
+     */
     int min_length() default 8;
+
+    /**
+     * Value define if user password must contain uppercase letters. Default value - false.
+     * @return {@link Boolean} constraints.
+     */
     boolean uppercase() default false;
+
+    /**
+     * Value define if user password must contain lowercase letters. Default value - true.
+     * @return {@link Boolean} constraints.
+     */
     boolean lowercase() default true;
+
+    /**
+     * Value define if user password must contain numbers. Default value - false.
+     * @return {@link Boolean} constraints.
+     */
     boolean numbers() default false;
+
+    /**
+     * Value define if user password must contain special characters e.g. "!@#$%^&". Default value - false.
+     * @return {@link Boolean} constraints.
+     */
     boolean special() default false;
 }
