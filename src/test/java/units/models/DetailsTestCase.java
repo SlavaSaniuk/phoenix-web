651a24e9c9748edfaf37ae7dad8dcc51d3bd7ae6
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-07-17 16:03:46 +0300

    Unit tests for Detail entity / Registration form

---
+++ src/test/java/units/models/DetailsTestCase.java
@@ -0,0 +1,55 @@
+package units.models;
+
+import com.phoenix.models.UserDetail;
+import com.phoenix.models.forms.RegistrationForm;
+import com.phoenix.webmvc.formatters.LocalDateFormatter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+class DetailsTestCase {
+
+    @Test
+    void constructor_validRegForm_shouldReturnNewUserDetail() {
+
+        final RegistrationForm form = new RegistrationForm();
+        form.setfName("test");
+        form.setlName("test2");
+        form.setBirthDay(LocalDate.now());
+        form.setSex("f");
+
+        UserDetail test = new UserDetail(form);
+
+        Assertions.assertEquals("test", test.getUserFname());
+        Assertions.assertEquals("test2", test.getUserLname());
+    }
+
+    @Test
+    void setUserBirthday_localDateNull_shouldSetAgeToZero() {
+
+        UserDetail test = new UserDetail();
+        test.setUserBirthday(null);
+
+        Assertions.assertEquals(0, test.getUserAge());
+
+    }
+
+    @Test
+    void setUserBirthday_localDateInvalid_shouldSetAgeToZero() {
+
+        UserDetail test = new UserDetail();
+        test.setUserBirthday(LocalDateFormatter.INVALID);
+
+        Assertions.assertEquals(0, test.getUserAge());
+    }
+
+    @Test
+    void setUserBirthday_newLocaldate_shouldSetUserAge() {
+
+        UserDetail test = new UserDetail();
+        test.setUserBirthday(LocalDate.of(2016, 9, 10));
+
+        Assertions.assertEquals(2, test.getUserAge());
+    }
+}
--- src/test/java/units/models/RegistrationFormTestCase.java
+++ src/test/java/units/models/RegistrationFormTestCase.java
@@ -1,11 +1,15 @@
 package units.models;
 
 import com.phoenix.models.Account;
+import com.phoenix.models.UserDetail;
 import com.phoenix.models.forms.RegistrationForm;
+import com.phoenix.webmvc.formatters.LocalDateFormatter;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 class RegistrationFormTestCase {
 
@@ -44,5 +48,41 @@ class RegistrationFormTestCase {
 
         Assertions.assertNull(this.form.getPassword());
 
+    }
+
+    @Test
+    void createUserDetail_allFieldsAreValid_shouldReturnDetailEntity() {
+        this.form.setfName("fname");
+        this.form.setlName("lname");
+        this.form.setBirthDay(LocalDate.now());
+        this.form.setSex("F");
+
+        UserDetail test = this.form.createUserDetail();
+
+        Assertions.assertEquals("fname", test.getUserFname());
+        Assertions.assertEquals(0, test.getUserAge());
+    }
+
+    @Test
+    void createUserDetail_birthdayIsInvalid_shouldSetUserAgeToZero() {
+
+        this.form.setBirthDay(LocalDateFormatter.INVALID);
+        this.form.setSex("Female");
+
+        UserDetail test = this.form.createUserDetail();
+
+        Assertions.assertEquals(0, test.getUserAge());
+        Assertions.assertEquals('F', test.getUserSex());
+    }
+
+
+    @Test
+    void createUserDetail_allFieldsAreValid_shouldCalculateUserAge() {
+        this.form.setBirthDay(LocalDate.of(1997, 1, 10));
+        this.form.setSex("Male");
+        UserDetail test = this.form.createUserDetail();
+
+        Assertions.assertEquals(22, test.getUserAge());
+        Assertions.assertEquals('M', test.getUserSex());
     }
 }
